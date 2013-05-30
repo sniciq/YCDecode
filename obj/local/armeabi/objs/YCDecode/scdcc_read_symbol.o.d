3f99b8afcd2cea3d417681e317c998cfd5da43b7
sniciq <sniciq@gmail.com>
2013-05-30 09:41:59 +0800

    去四点

--- obj/local/armeabi/objs/YCDecode/scdcc_read_symbol.o.d
+++ obj/local/armeabi/objs/YCDecode/scdcc_read_symbol.o.d
@@ -2,26 +2,26 @@ obj/local/armeabi/objs/YCDecode/scdcc_read_symbol.o: \
  jni/scdcc_read_symbol.cpp jni/scdcc_errs.h jni/SCDCC_Symbol.h \
  jni/basetypedef.h jni/scdcc_grid_analysis.h jni/scdcc_read_symbol.h \
  jni/scdcc_malloc.h \
- D:/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/malloc.h \
- D:/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
- D:/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
- D:/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/api-level.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stddef.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/features.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/host.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
- D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/string.h \
- D:/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/../include/string.h
+ D:/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/malloc.h \
+ D:/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ D:/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stddef.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+ D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/string.h \
+ D:/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/../include/string.h
 
 jni/scdcc_errs.h:
 
@@ -35,42 +35,42 @@ jni/scdcc_read_symbol.h:
 
 jni/scdcc_malloc.h:
 
-D:/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/malloc.h:
+D:/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/malloc.h:
 
-D:/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+D:/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
 
-D:/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+D:/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
 
-D:/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+D:/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/android/api-level.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stddef.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stddef.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/features.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/host.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
 
-D:/android-ndk-r8e/sources/cxx-stl/stlport/stlport/string.h:
+D:/android-ndk-r8d/sources/cxx-stl/stlport/stlport/string.h:
 
-D:/android-ndk-r8e/platforms/android-9/arch-arm/usr/include/../include/string.h:
+D:/android-ndk-r8d/platforms/android-9/arch-arm/usr/include/../include/string.h:
